cb53c030d065f7ed13e5f13463f322dbddd94ab5
Ana Karoline <vieirakarol302@gmail.com>
2026-07-06 17:24:39 -0400

    teste

--- Semana04_Hyperautomation/portal_fake/cadastro.py
+++ Semana04_Hyperautomation/portal_fake/cadastro.py
@@ -1,1 +1,21 @@
-#arquivo de teste da funcionalidade
+# cadastro.py
+
+usuarios = []
+
+def cadastrar_usuario(nome, email):
+    usuario = {
+        "nome": nome,
+        "email": email
+    }
+
+    usuarios.append(usuario)
+
+    print("Usuário cadastrado com sucesso!")
+    print(usuario)
+
+
+# Teste
+cadastrar_usuario("Maria Silva", "maria@email.com")
+
+print("\nLista de usuários:")
+print(usuarios)
